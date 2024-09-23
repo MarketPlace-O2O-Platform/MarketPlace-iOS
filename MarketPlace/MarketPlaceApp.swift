f4d6bb6464330aa4e2f8a2fb7d17023c5ee3890a
yena0213 <yeana3131@naver.com>
2024-09-23 15:44:30 +0900

    Initial Commit

---
+++ MarketPlace/MarketPlaceApp.swift
@@ -0,0 +1,17 @@
+//
+//  MarketPlaceApp.swift
+//  MarketPlace
+//
+//  Created by 이예나 on 9/23/24.
+//
+
+import SwiftUI
+
+@main
+struct MarketPlaceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
