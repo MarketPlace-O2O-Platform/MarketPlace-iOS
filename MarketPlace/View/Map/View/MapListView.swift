2b78a068574a85c3e5d0e97be242e745428a10f6
blwxnhan <rabbitlin@naver.com>
2025-05-27 20:53:45 +0900

    [Docs] 폴더명 변경 및 폴더 위치 변경

--- MarketPlace/View/Map/View/MapListView.swift
+++ MarketPlace/View/Map/View/MapListView.swift
@@ -1,7 +1,7 @@
 import SwiftUI
 
 struct MapListView: View {
-    @ObservedObject var marketVM: MarketCategoryDetailViewModel // ViewModel을 전달받음
+    @ObservedObject var marketVM: MarketCategoryDetailViewModel
     @State private var selectedIndex: Int?
     
     var body: some View {
