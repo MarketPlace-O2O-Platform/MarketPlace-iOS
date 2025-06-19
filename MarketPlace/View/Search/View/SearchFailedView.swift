7f1d61831bfce0978330540c5bf786fa68dda53d
yena0213 <yeana3131@naver.com>
2025-06-19 17:38:59 +0900

    [Feat] page connect - searchview & requestView

--- MarketPlace/View/Search/View/SearchFailedView.swift
+++ MarketPlace/View/Search/View/SearchFailedView.swift
@@ -1,16 +1,9 @@
-//
-//  searchIgnore.swift
-//  MarketPlace
-//
-//  Created by 이예나 on 6/4/25.
-//
-
 import SwiftUI
 
 struct SearchFailedView: View {
     var body: some View {
         VStack {
-            VStack{
+            VStack {
                 Text("검색 결과가 없어요.")
                 Text("찾으시는 매장이 없으신가요?")
             }
@@ -26,7 +19,7 @@ struct SearchFailedView: View {
                 .frame(width: 302, height: 185)
                 .padding(.top, 40)
             
-            Button(action: {}) {
+            NavigationLink(destination: RequestMainView()) {
                 Text("요청하기")
                     .frame(width: 240, height: 38)
                     .foregroundColor(.white)
@@ -40,8 +33,4 @@ struct SearchFailedView: View {
         .frame(maxWidth: .infinity, maxHeight: .infinity)
         .background(Colors.gray_100)
     }
-}
-
-#Preview {
-    SearchFailedView()
 }
