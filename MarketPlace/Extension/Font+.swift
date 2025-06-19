9384a72c8e356cc46e5b43fade35692233eaeb85
blwxnhan <rabbitlin@naver.com>
2025-06-19 14:43:36 +0900

    [Feat] 폰트 추가 및 Extension 구현

---
+++ MarketPlace/Extension/Font+.swift
@@ -0,0 +1,29 @@
+//
+//  Font+.swift
+//  MarketPlace
+//
+//  Created by Bowon Han on 6/19/25.
+//
+
+import SwiftUI
+
+extension Font {
+    static func pretendard(_ size: CGFloat, weight: Weight = .regular) -> Font {
+        let fontName: String
+        
+        switch weight {
+        case .ultraLight: fontName = "Pretendard-Thin"
+        case .thin: fontName = "Pretendard-ExtraLight"
+        case .light: fontName = "Pretendard-Light"
+        case .regular: fontName = "Pretendard-Regular"
+        case .medium: fontName = "Pretendard-Medium"
+        case .semibold: fontName = "Pretendard-SemiBold"
+        case .bold: fontName = "Pretendard-Bold"
+        case .heavy: fontName = "Pretendard-ExtraBold"
+        case .black: fontName = "Pretendard-Black"
+        default: fontName = "Pretendard-Regular"
+        }
+        
+        return .custom(fontName, size: size)
+    }
+}
