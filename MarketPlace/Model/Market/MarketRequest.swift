5be8952ac20d2d35f06b741dc5dbb7aec0ede01c
yena0213 <yeana3131@naver.com>
2025-06-19 16:37:49 +0900

    [Feat] creat MarketRequestModel

--- MarketPlace/Model/Market/MarketRequest.swift
+++ MarketPlace/Model/Market/MarketRequest.swift
@@ -1,1 +1,27 @@
+//
+//  MarketRequest.swift
+//  MarketPlace
+//
+//  Created by 이예나 on 6/19/25.
+//
 
+import Foundation
+
+struct MarketRequestResponse: Codable {
+    let content: [MarketRequest]
+    let page: MarketRequestPage
+}
+
+struct MarketRequest: Codable, Identifiable {
+    let id: Int
+    let name: String
+    let address: String
+    let count: Int
+}
+
+struct MarketRequestPage: Codable {
+    let size: Int
+    let number: Int
+    let totalElements: Int
+    let totalPages: Int
+}
