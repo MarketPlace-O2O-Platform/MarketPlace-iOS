c564ccfa3ef00dcd4c7917cf4b5e5b37ff213e0d
yena0213 <yeana3131@naver.com>
2025-06-26 18:28:39 +0900

    [Design#28] create CheerSearchfailedView

---
+++ MarketPlace/View/Cheer/View/CheerSearchfailedView.swift
@@ -0,0 +1,50 @@
+//
+//  CheerSearchfailedView.swift
+//  MarketPlace
+//
+//  Created by 이예나 on 6/26/25.
+//
+
+import SwiftUI
+
+struct CheerSearchfailedView: View {
+    var body: some View {
+        VStack{
+            VStack(alignment: .center) {
+                Text("검색 결과가 없어요.")
+                Text("찾으시는 매장이 없으신가요?")
+            }
+            .foregroundColor(Colors.gray_700)
+            .font(.custom("Pretendard-Medium", size: 14))
+            .padding(.top, 30)
+         
+            Text("매장 요청하기를 해보세요!")
+                .foregroundColor(Colors.primary)
+                .font(.custom("Pretendard-SemiBold", size: 24))
+                .padding(.top, 96)
+            
+            Image("searchIgnore")
+                .padding(.top, 30)
+            
+            Button(action: {
+                //버튼 액션
+            }) {
+                Text("요청하기")
+                    .foregroundColor(.white)
+                    .font(.custom("Pretendard-Bold", size: 14))
+                    .frame(width: 240, height: 38)
+                    .background(
+                        RoundedCorner(radius: 4)
+                            .fill(Colors.primary)
+                    )
+            }
+            .padding(.top, 20)
+            
+            Spacer()
+        }
+    }
+}
+
+#Preview {
+    CheerSearchfailedView()
+}
