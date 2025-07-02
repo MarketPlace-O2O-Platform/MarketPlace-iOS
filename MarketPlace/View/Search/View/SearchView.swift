82407df8e3e62502f2bdf286b769dadf9b1ff191
yena0213 <yeana3131@naver.com>
2025-07-02 17:15:47 +0900

    [Feat] SearchView -> MarketDetailView 연결

--- MarketPlace/View/Search/View/SearchView.swift
+++ MarketPlace/View/Search/View/SearchView.swift
@@ -58,7 +58,6 @@ struct SearchView: View {
                 } else {
                 if hasData {
                     SearchSecondView(
-                        //                        searchText: $viewModel.searchText,
                         viewModel: viewModel
                     )
                 } else{
@@ -89,34 +88,4 @@ struct SearchView: View {
            }
         }
     }
-    
-//    // 최근 검색어 새로고침 함수
-//    private func refreshRecentSearches() {
-//        DispatchQueue.main.async {
-//            self.recentSearches = UserDefaults.standard.stringArray(forKey: "recentSearches") ?? []
-//        }
-//    }
-//    
-//    // 최근 검색어 추가 함수에 로그 추가
-//    private func addToRecentSearches(_ searchQuery: String) {
-//            let trimmedQuery = searchQuery.trimmingCharacters(in: .whitespacesAndNewlines)
-//            guard !trimmedQuery.isEmpty else { return }
-//        
-//            print("🔍 검색어 추가: \(trimmedQuery)")
-//            print("🔍 이전 배열: \(recentSearches)")
-//            
-//            // 중복 제거 후 맨 앞에 추가
-//            var updatedSearches = [trimmedQuery] + recentSearches.filter { $0 != trimmedQuery }
-//            
-//            // 최대 10개까지만 저장
-//            if updatedSearches.count > 10 {
-//                updatedSearches.removeLast()
-//            }
-//            
-//            // @State 배열 업데이트 (즉시 UI 반영)
-//            recentSearches = updatedSearches
-//            
-//            // UserDefaults에 저장 (영구 저장)
-//            UserDefaults.standard.set(updatedSearches, forKey: "recentSearches")
-//        }
 }
