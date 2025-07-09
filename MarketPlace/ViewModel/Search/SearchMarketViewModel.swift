e025d7e6299d20ea448effc6b4ca906806024390
blwxnhan <rabbitlin@naver.com>
2025-07-09 13:58:48 +0900

    [Feat] 검색화면 페이징 구현

--- MarketPlace/ViewModel/Search/SearchMarketViewModel.swift
+++ MarketPlace/ViewModel/Search/SearchMarketViewModel.swift
@@ -16,14 +16,24 @@ final class SearchMarketViewModel: ObservableObject {
     @Published var market: [MarketSearchModel] = []
     @Published var popularCoupon: [CouponTopModel] = []
     
-    init(marketService: MarketServiceProtocol = MarketService(),
-    couponService: CouponServiceProtocol = CouponService()) {
+    @Published var lastPageIndex: Int?
+    @Published var currentKeyword: String = ""
+
+    var currentPage: Int = 1
+    var isLoading: Bool = false
+    var hasNextPage: Bool = true
+    
+    init(
+        marketService: MarketServiceProtocol = MarketService(),
+        couponService: CouponServiceProtocol = CouponService()
+    ) {
         self.marketService = marketService
         self.couponService = couponService
         
         self.recentSearches = UserDefaults.standard.stringArray(forKey: "recentSearches") ?? []
     }
     
+    // MARK: - 최근 검색어 추가 메서드 (UserDefaults)
     func addRecentSearch(_ query: String) {
         let trimmed = query.trimmingCharacters(in: .whitespacesAndNewlines)
         guard !trimmed.isEmpty else { return }
@@ -35,38 +45,71 @@ final class SearchMarketViewModel: ObservableObject {
         UserDefaults.standard.set(updated, forKey: "recentSearches")
     }
     
+    // MARK: - 최근 검색어 삭제 메서드 (UserDefaults)
     func clearRecentSearches() {
         recentSearches = []
         UserDefaults.standard.set([], forKey: "recentSearches")
     }
 
+    // MARK: - 최근 검색어 불러오는 메서드 (UserDefaults)
     func reloadRecentSearches() {
         recentSearches = UserDefaults.standard.stringArray(forKey: "recentSearches") ?? []
     }
     
+    // MARK: - 검색 결과 불러오는 메서드
     @MainActor
-    func fetchMarkets(name: String) async -> Bool {
+    func fetchMarkets(
+        lastPageIndex: Int? = nil,
+        pageSize: Int? = nil,
+        keyword: String
+    ) async -> Bool {
         var hasData: Bool = true
         
+        if currentKeyword != keyword {
+            currentPage = 1
+            hasNextPage = true
+        }
+        
+        guard !isLoading, hasNextPage else { return false }
+        
+        isLoading = true
+        
         let result = await marketService.fetchSearchMarketsList(
-            lastPageIndex: nil,
-            pageSize: nil,
-            name: name
+            lastPageIndex: lastPageIndex,
+            pageSize: pageSize,
+            name: keyword
         )
         
         switch result {
         case .success(let data, _):
-            self.market = data.response.marketResDtos
+            if currentPage == 1 {
+                self.market = data.response.marketResDtos
+            } else {
+                self.market.append(contentsOf: data.response.marketResDtos)
+            }
+            
+            if let last = data.response.marketResDtos.last {
+                self.lastPageIndex = last.marketId
+            }
+            
+            self.hasNextPage = data.response.hasNext
+            currentPage += 1
+            
+//            self.market = data.response.marketResDtos
             if market.isEmpty {
                 hasData = false
             }
+            
         case .failure(let statusCode, let message):
             print("[MarketSearch] - [\(statusCode)]: \(message ?? "알 수 없는 오류")")
         }
         
+        isLoading = false 
+        
         return hasData
     }
     
+    // MARK: - 인기 쿠폰 불러오는 메서드
     func fetchPopularCoupon(pageSize: Int?) async {
         let result = await couponService.fetchCouponTopPopular(pageSize: pageSize)
         
@@ -76,5 +119,5 @@ final class SearchMarketViewModel: ObservableObject {
         case .failure(let statusCode, let message):
             print("[popularCoupon] - [\(statusCode)]: \(message ?? "알 수 없는 오류")")
         }
-            }
+    }
 }
