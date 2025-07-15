19e568bc0151fd0adb7c68faa85da4ec82bd50a5
yena0213 <yeana3131@naver.com>
2025-07-15 18:17:41 +0900

    [Feat] create Recipt Model

---
+++ MarketPlace/Model/ReciptModel.swift
@@ -0,0 +1,13 @@
+//
+//  ReciptModel.swift
+//  MarketPlace
+//
+//  Created by 이예나 on 7/15/25.
+//
+
+import Foundation
+
+struct ReciptModel: Codable {
+    let couponId: Int
+    var isUsed: Bool
+}
