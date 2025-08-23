dc23da6ac6cac9fc7c6eb22872bd2948e99ee92f
yena0213 <yeana3131@naver.com>
2025-08-23 17:20:26 +0900

    [chore] contentview() 테스트 링크 수정

--- MarketPlace/MarketPlaceApp.swift
+++ MarketPlace/MarketPlaceApp.swift
@@ -15,11 +15,7 @@ struct MarketPlaceApp: App {
 
     var body: some Scene {
         WindowGroup {
-            if loginVM.isLoggedIn {
-                AlertView()
-            } else {
-                LoginView()
-            }
+            ContentView()
         }
         .environmentObject(loginVM)
     }
