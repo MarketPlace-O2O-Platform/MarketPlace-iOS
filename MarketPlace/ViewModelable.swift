82f7bc87c33662a3e5ca00479989142fd4139c4a
blwxnhan <rabbitlin@naver.com>
2025-10-02 11:34:23 +0900

    [Feat] ViewModelable 추가

---
+++ MarketPlace/ViewModelable.swift
@@ -0,0 +1,17 @@
+//
+//  ViewModelable.swift
+//  MarketPlace
+//
+//  Created by Bowon Han on 10/2/25.
+//
+
+import Foundation
+
+protocol ViewModelable: ObservableObject {
+  associatedtype Action
+  associatedtype State
+  
+  var state: State { get }
+  
+  func action(_ action: Action)
+}
