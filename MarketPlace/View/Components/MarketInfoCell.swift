2e6554d517ac84306c72054c0447da8ad6def32b
blwxnhan <rabbitlin@naver.com>
2025-11-17 19:56:16 +0900

    [Fix] MarketInfoCell 찜 버튼 비로그인 시 비활성화

--- MarketPlace/View/Components/MarketInfoCell.swift
+++ MarketPlace/View/Components/MarketInfoCell.swift
@@ -2,6 +2,7 @@ import SwiftUI
 
 struct MarketInfoCell: View {
     @ObservedObject var viewModel: MarketInfoCellViewModel
+    @EnvironmentObject var loginViewModel: LoginViewModel
     @State var isBookmarked: Bool
 
     init(isBookmarked: Bool, viewModel: MarketInfoCellViewModel) {
@@ -19,19 +20,22 @@ struct MarketInfoCell: View {
                 width: 110,
                 height: 110
             )
-            
             .frame(width: 110, height: .infinity)
             .clipShape(RoundedRectangle(cornerRadius: 4))
 
             VStack(alignment: .leading) {
                 Text(viewModel.marketData.marketName)
+                    .lineLimit(1)
                     .pretendardFont(size: 16, weight: .semibold)
                     .foregroundColor(Color(hex: "333333"))
+                    .padding(.bottom, 3)
 
                 Text(viewModel.marketData.marketDescription)
+                    .multilineTextAlignment(.leading)
+                    .lineLimit(2)
                     .pretendardFont(size: 13, weight: .medium)
                     .foregroundColor(Color(hex: "7D7D7D"))
-                    .multilineTextAlignment(.leading)
+                    .padding(.bottom, 10)
                 
                 Spacer()
 
@@ -43,23 +47,25 @@ struct MarketInfoCell: View {
                     Text(viewModel.marketData.address)
                         .pretendardFont(size: 13, weight: .medium)
                         .foregroundColor(Color(hex: "333333"))
+                    
                     Spacer()
 
-                    Button(action: {
-                        Task {
-                            await viewModel.postFavoriteMarket(marketId: viewModel.marketData.id)
+                    if loginViewModel.isLoggedIn {
+                        Button(action: {
+                            Task {
+                                await viewModel.postFavoriteMarket(marketId: viewModel.marketData.id)
+                            }
+                        }) {
+                            Image(systemName: viewModel.marketData.isFavorite ? "bookmark.fill" : "bookmark")
+                                .resizable()
+                                .frame(width: 14, height: 20)
+                                .foregroundColor(Color(hex: "#4B4B4B"))
                         }
-                    }) {
-                        Image(systemName: viewModel.marketData.isFavorite ? "bookmark.fill" : "bookmark")
-                            .resizable()
-                            .frame(width: 14, height: 20)
-                            .foregroundColor(Color(hex: "#4B4B4B"))
                     }
                 }
             }
-            .padding(.leading, 10)
+            .padding([.leading, .trailing], 10)
             .padding(5)
-            .frame(maxHeight: 110)
         }
         .padding(15)
     }
