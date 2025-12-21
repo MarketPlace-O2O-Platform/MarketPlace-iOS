f3c861a25dbffe537b99abd3cd4b07ae96eaf7b7
yena0213 <yeana3131@naver.com>
2025-12-21 23:54:22 +0900

    [Fix] 공감 검색 화면 검색 안되는거 해결

--- MarketPlace/Model/CheerMarketModel.swift
+++ MarketPlace/Model/CheerMarketModel.swift
@@ -4,15 +4,19 @@ struct CheerMarketModel: Codable, Identifiable {
     let marketId: Int
     let marketName: String
     let marketDescription: String?
-    let thumbnail: String
-    var cheerCount: Int
+    let thumbnail: String?
+    var cheerCount: Int?
     var isCheer: Bool
     var dueDate: Int?
-    
+
     var id: Int { marketId }
-    
+
     var dueDateFormmater: Int {
         guard let dueDate = dueDate else { return 0 }
         return dueDate
+    }
+
+    var thumbnailPath: String {
+        thumbnail ?? ""
     }
 }
--- MarketPlace/ViewModel/Cheer/CheerCardCellViewModel.swift
+++ MarketPlace/ViewModel/Cheer/CheerCardCellViewModel.swift
@@ -25,20 +25,20 @@ final class CheerCardCellViewModel: ObservableObject {
     // MARK: - 공감탭 매장 공감 API
     func postCheerMarket(tempMarketId: Int) async -> Bool {
         let result = await cheerMarketService.postCheerMarket(tempMarketId: tempMarketId)
-        
+
         switch result {
         case .success(let data, let statusCode):
             if case 200..<300 = statusCode {
                 self.isCheer = true
                 self.cheerMarket.cheerCount = data.response.cheerCount
             }
-            
+
             return true
-            
+
         case .failure(let statusCode, let message):
             print("[postCheerMarket] - [\(statusCode)]: \(message ?? "알 수 없는 오류")")
         }
-        
+
         return false
     }
 }
