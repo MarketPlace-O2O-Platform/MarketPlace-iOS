46e08bdf198c1640ec8141995622b738d3e97ebe
blwxnhan <rabbitlin@naver.com>
2026-01-24 16:41:37 +0900

    [Refactor] CouponModel Entity 추가

--- MarketPlace/Data/Network/DTO/Coupon/CouponResDto.swift
+++ MarketPlace/Data/Network/DTO/Coupon/CouponResDto.swift
@@ -10,15 +10,15 @@ import Foundation
 /// - NOTE: New,Popluar 쿠폰 Model에 공통적으로 포함되어있는 필드를 담은 BasicModel입니다
 /// 
 struct CouponResDto: Codable, Identifiable {
-    var couponId: Int
-    var couponName: String
-    var marketId: Int
-    var marketName: String
-    var address: String
-    var thumbnail: String
-    var isAvailable: Bool
+    let couponId: Int
+    let couponName: String
+    let marketId: Int
+    let marketName: String
+    let address: String
+    let thumbnail: String
+    let isAvailable: Bool
     var isMemberIssued: Bool
-    var couponType: String
+    let couponType: String
     
     var id: Int { couponId }
 }
---
+++ MarketPlace/Domain/Entities/Coupon/CouponModel.swift
@@ -0,0 +1,20 @@
+//
+//  CouponModel.swift
+//  MarketPlace
+//
+//  Created by Bowon Han on 1/23/26.
+//
+
+import Foundation
+
+struct CouponModel: Identifiable {
+    let id: Int
+    let name: String
+    let marketId: Int
+    let marketName: String
+    let thumbnail: String
+    let address: String
+    let isMemberIssued: Bool
+    let description: String
+    let isAvailable: Bool
+}
