c8eea4abed519e158b5a908375d86ae56c71dc8c
blwxnhan <rabbitlin@naver.com>
2026-03-12 13:59:07 +0900

    [Feat] 카테고리별 매장 조회 UseCase 추가

--- MarketPlace/Domain/UseCase/FetchMarketWithCategoryUseCase.swift
+++ MarketPlace/Domain/UseCase/FetchMarketWithCategoryUseCase.swift
@@ -8,17 +8,41 @@
 import Foundation
 
 protocol FetchMarketWithCategoryUseCase {
-    func execute(category: MarketCategory) -> [MarketListModel]
+    func execute(category: MarketCategory, reset: Bool) async -> Result<[MarketListModel], Error>
 }
 
 final class DefaultFetchMarketWithcategoryUseCase: FetchMarketWithCategoryUseCase {
     private let marketRepository: MarketRepository
     
+    private var lastMarketId: Int?
+    private var currentPage: Int = 1
+    private var currentCategory: MarketCategory?
+    
     init(marketRepository: MarketRepository) {
         self.marketRepository = marketRepository
     }
     
-    func execute(category: MarketCategory) -> [MarketListModel] {
+    func execute(category: MarketCategory, reset: Bool) async -> Result<[MarketListModel], Error> {
+        if let currentCategory = currentCategory,
+            reset || (currentCategory != category) {
+            currentPage = 1
+            lastMarketId = nil
+        }
         
+        currentCategory = category
+        
+        let result = await marketRepository.fetchMarketWithCategory(category: category, lastPageIndex: lastMarketId, pageSize: 10)
+                
+        switch result {
+        case .success(let data):
+            
+            lastMarketId = data.last?.id
+            currentPage += 1
+            
+            return .success((data))
+            
+        case .failure(let error):
+            return .failure(error)
+        }
     }
 }
